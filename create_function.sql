2a014946e0df648c6adb28d706d5786b7e4c2491
Sinward Ye <244649399@qq.com>
2019-02-01 13:40:16 +0800

    增加了保存RAWCONTENT表的函数。使用数据库存储过程而非在python里面直接写，可以增加安全性和性能。

--- create_function.sql
+++ create_function.sql
@@ -17,6 +17,21 @@ BEGIN
     DROP TABLE CONTENT CASCADE;
     DROP FUNCTION INITUNGET(amount int);
     DROP FUNCTION UNINIT();
+    DROP FUNCTION SAVERAWCONTENT
+    (_docid int,_cid int,_categoryid int,_content text);
+    RETURN TRUE;
+END;
+$$
+LANGUAGE PLPGSQL;
+
+CREATE FUNCTION SAVERAWCONTENT
+(_docid int,_cid int,_categoryid int,_content text)
+RETURNS boolean
+--保存一个RAWCONTENT
+AS $$
+BEGIN
+    INSERT INTO RAWCONTENT VALUES(_docid,_cid,_categoryid,_content);
+    DELETE FROM UNGET WHERE docid=_docid;
     RETURN TRUE;
 END;
 $$
