884033252eb49ac6518acfa58f556ae07f020a1d
Sinward Ye <244649399@qq.com>
2019-02-01 13:39:19 +0800

    增加了解析器，同时将web.get改为返回responese，以增加通用性

--- base.py
+++ base.py
@@ -4,8 +4,11 @@
 
 import requests
 import config
+import bs4
+from bs4 import BeautifulSoup
+import re
 
-class web:
+class Web:
     @staticmethod
     def get(url,**kvargs):
         arg=config.requests_arg
@@ -18,10 +21,40 @@ class web:
             try:
                 r=requests.get(url,timeout=timeout)
                 r.raise_for_status()
-                return r.text
+                return r
             except requests.exceptions.Timeout:
                 print('web.get:timeout retry:\n%s'%url)
                 retry+=1
             except Exception as e:
                 raise e
         raise Exception('web.get:retry times larger than max_retry')
+
+class Praser:
+    '''
+    解析器
+    主要存储各种解析函数
+    '''
+    def __init__(self):
+        self.cid_pattern=re.compile(r'cid=\d+')
+        self.categoryid_pattern=re.compile(r'categoryId=\d+')
+
+    def get_raw(self,r):
+        '''
+        从页面数据中，获取cid,categoryid,content
+        args:
+            r 返回的responese requests.responese对象
+        returns:
+            cid int
+            categoryid int
+            content str
+        '''
+        url=r.url
+        print('url=%s'%url)
+        cid=int(self.cid_pattern.search(url).group(0).split('=')[1])
+        categoryid=int(self.categoryid_pattern.search(url).group(0).split('=')[1])
+
+        soup=BeautifulSoup(r.text,'lxml')
+        body=soup.find('body',attrs={'id':'termBody'})
+        content=body.find('div',attrs={'id':'content'})
+
+        return (cid,categoryid,str(content))
